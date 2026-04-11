e05f65a60df7f6664ee2363a2211caad3decc23d
Y07-py <eruhu07@gmail.com>
2026-04-11 13:00:50 +0900

    first commit

---
+++ Growl/Growl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Growl
+//
+//  Created by 木本瑛介 on 2026/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Growl/Growl/GrowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowlApp.swift
+//  Growl
+//
+//  Created by 木本瑛介 on 2026/04/11.
+//
+
+import SwiftUI
+
+@main
+struct GrowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
